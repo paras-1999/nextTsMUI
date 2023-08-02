09420a3bbdd5067a1d578e7209f08ba2d5b4de56
paras-1999 <paras.saxena@neosoftmail.com>
2023-08-02 12:26:27 +0530

    setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import { Button } from '@mui/material'
 
 export default function Home() {
   return (
     <main className={styles.main}>
+      <Button variant="contained">Text</Button>
       <div className={styles.description}>
         <p>
           Get started by editing&nbsp;
